181ee5f2c573c748f5df779f63bb129bff3bfe81
Aditya <139672646+lovedarkcode@users.noreply.github.com>
2024-03-02 11:38:15 +0530

    Add files via upload

---
+++ demo/src/demo/demo.java
@@ -0,0 +1,61 @@
+package demo;
+
+public class demo {
+	public static void main(String[] args) {
+//		System.out.println("Hello World");
+//		System.out.println("Aditya");
+//		System.out.println("ABC");
+		
+		int number1=100;
+		int number2 =200;
+//		int add = number1 + number2;
+//		int sub = number1-number2;
+//		int mul = number1*number2;
+//		int mod = number1%number2;
+//		
+//		System.out.println("Add of "+number1+" and "+number2+" is = "+add);
+//		System.out.println("Sub of "+number1+" and "+number2+" is = "+sub);
+//		System.out.println("Mul of "+number1+" and "+number2+" is = "+mul);
+//		System.out.println("Mod of "+number1+" and "+number2+" is = "+mod);
+		
+//		Operator
+//		number1++;
+//		System.out.println("the increment value of no1 is ="+number1);
+//		number2--;
+//		System.out.println("The decrement value of no2 is="+number2);
+//		System.out.println("the equal to operator is="+(number1==number2));
+		
+//		System.out.println("the  not equal to operator is="+(number1!=number2));
+//		System.out.println(number1>number2);
+//		System.out.println(number1<number2);
+//		System.out.println(number1>=number2);
+//		System.out.println(number1<=number2);
+
+//     Logical Operators
+//		AND
+		
+//		System.out.println((number1==number2)&&(number1>number2));
+//		System.out.println((number1!=number2)&&(number1>number2));
+//		System.out.println((number1==number2)&&(number1<number2));
+//		System.out.println((number1!=number2)&&(number1<number2));
+		
+//		OR
+		
+//		System.out.println((number1==number2)||(number1>number2));
+//		System.out.println((number1!=number2)||(number1>number2));
+//		System.out.println((number1==number2)||(number1<number2));
+//		System.out.println((number1!=number2)||(number1<number2));
+		
+//		NOT
+		
+		System.out.println(!(number1>number2));
+		System.out.println(!(number1<number2));
+		System.out.println(!(number1!=number2));
+		
+		
+		
+		
+		
+	}
+
+}
