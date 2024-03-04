61a47da1880089484a3298992a51366757ecd66c
Aditya <139672646+lovedarkcode@users.noreply.github.com>
2024-03-04 16:54:24 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        // System.out.println("Please enter the name");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // Addition
+
+        // int number1, number2, number3;
+        // System.out.println("Enter the first number=");
+        // Scanner sc1 = new Scanner(System.in);
+        // number1 = sc1.nextInt();
+        // System.out.println("Enter the second number=");
+        // Scanner sc2 = new Scanner(System.in);
+        // number2 = sc2.nextInt();
+        // number3 = number1 + number2;
+        // System.out.println("The additio of two numbers is=" + number3);
+
+        // Substraction
+
+        // int number1, number2, number3;
+        // System.out.println("Enter the first number=");
+        // Scanner n1 = new Scanner(System.in);
+        // number1 = n1.nextInt();
+        // System.out.println("Enter the second number=");
+        // Scanner n2 = new Scanner(System.in);
+        // number2 = n2.nextInt();
+        // number3 = number1 - number2;
+        // System.out.println("The substraction of two numbers is=" + number3);
+
+        // Mutiplication
+
+        // int number1, number2, number3;
+        // System.out.println("Enter the first number=");
+        // Scanner m1 = new Scanner(System.in);
+        // number1 = m1.nextInt();
+        // System.out.println("Enter the second number=");
+        // Scanner m2 = new Scanner(System.in);
+        // number2 = m2.nextInt();
+        // number3 = number1 * number2;
+        // System.out.println("The multiplication of two numbers is=" + number3);
+
+        // Division
+
+        int number1, number2, number3;
+        System.out.println("Enter the first number=");
+        Scanner d1 = new Scanner(System.in);
+        number1 = d1.nextInt();
+        System.out.println("Enter the second number=");
+        Scanner d2 = new Scanner(System.in);
+        number2 = d2.nextInt();
+        number3 = number1 / number2;
+        System.out.println("The division of two numbers is=" + number3);
+
+    }
+
+}
