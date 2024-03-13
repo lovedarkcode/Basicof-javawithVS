159706ef73ebac84ea0d1da840e5007a4ea2a6f9
Aditya <139672646+lovedarkcode@users.noreply.github.com>
2024-03-13 15:25:29 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,28 @@
+public class loop {
+    public static void main(String[] args) {
+        // using while loop
+        int count = 0;
+        while (count <= 4) {
+            System.out.println("Aditya Upaganlawar");
+            count++;
+        }
+        // using for loop
+        String str = "Aditya Upaganlawar";
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(str);
+        }
+        // using while loop
+
+        int num = 1;
+        while (num <= 10) {
+            System.out.println(num + " ");
+            num++;
+        }
+        // using for loop
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
