488e91c1ae2bd4d64dbfa914fe95d600550bd22b
Aditya <139672646+lovedarkcode@users.noreply.github.com>
2024-04-09 07:52:21 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,14 @@
+public class methods {
+    static void addition(intf first, int second) {
+        int third;
+        third = first + second;
+        System.out.println("Addition is=" + third);
+    }
+
+    public static void main(String[] args) {
+        addition(3, 8);
+        addition(12, 5);
+        addition(7, 9);
+    }
+
+}
